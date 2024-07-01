4a41ac4233f8aa0c43c3bea22e8fe4e9b095e3bd
Jaspher0406 <jtocfemia@techfactors.com>
2024-07-01 23:04:52 +0800

    test

---
+++ src/layouts/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+function MainContainer() {
+  return (
+    <Container className='main-container'>
+      
+    </Container>
+  )
+}
+
+export default MainContainer
