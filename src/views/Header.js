194c1a471ecda51e5b5f83b6a7ffb98a860e1029
Jaspher0406 <jtocfemia@techfactors.com>
2024-09-27 16:33:07 +0800

    hahaha

--- src/views/Header.js
+++ src/views/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { MenuOutlined, UserOutlined } from '@ant-design/icons'
+import { MenuOutlined, UserOutlined, RollbackOutlined, TagsOutlined, GlobalOutlined } from '@ant-design/icons'
 
 export default function Header() {
   const [userDropdown, setUserDropdown] = useState(false)
@@ -8,16 +8,16 @@ export default function Header() {
     <>
       <div className='header'>
         <MenuOutlined className='menu-icon' />
-        <span>PAGE NI ISJI</span>
+        <h1>ISJI NEW FM</h1>
         <div className='user-btn' onClick={() => setUserDropdown(!userDropdown)}>
           <UserOutlined className='menu-icon' />
           {userDropdown &&
             <div className='user-dropdown'>
-              <div className='dropdown-settings'>My Account</div>
+              <div className='dropdown-settings'>My Account <UserOutlined /></div>
               <hr />
-              <div className='dropdown-settings'>News Articles</div>
-              <div className='dropdown-settings'>Saved News</div>
-              <div className='dropdown-settings'>Logout</div>
+              <div className='dropdown-settings'>News Articles <GlobalOutlined /></div>
+              <div className='dropdown-settings'>Saved News <TagsOutlined /></div>
+              <div className='dropdown-settings'>Logout <RollbackOutlined /></div>
             </div>
           }
         </div>
