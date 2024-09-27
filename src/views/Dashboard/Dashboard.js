75980fa6d948f3b7719c5d960d93731144bb1b51
Jaspher0406 <jtocfemia@techfactors.com>
2024-09-27 16:23:57 +0800

    test commit

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,9 +1,70 @@
-import React from 'react'
-import MainContainer from '../../layouts/MainContainer'
+import React, { useEffect, useState } from 'react';
+import MainContainer from '../../layouts/MainContainer';
+import { Image, Card, Flex } from 'antd';
+import { Spin } from "antd";
+import { LoadingOutlined } from '@ant-design/icons';
+const { Meta } = Card;
+
 
 export default function Dashboard() {
+  const [latestNews, setLatestNews] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchLatestNews = async () => {
+      try {
+        const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a62d3cbd9a34497abeb4870d4eadf56f');
+        setLoading(true)
+        if (response.ok) {
+          const data = await response.json();
+          setLatestNews(data.articles);
+          setLoading(false)
+        } else {
+          console.error('Error fetching news:', response.statusText);
+          setLoading(false)
+        }
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    };
+
+    fetchLatestNews();
+  }, []);
+
+  console.log({ latestNews })
+
   return (
     <MainContainer>
+      {!loading ?
+        <div className='dashboard-cards'>
+          {latestNews.map(item => {
+            return (
+              <Card
+                hoverable
+                style={{
+                  width: 350,
+                }}
+                cover={<Image alt="example" className='card-images' src={item.urlToImage == null ? 'No data available' : item.urlToImage} />}
+              >
+                <Meta title={item.title} description={item.description} />
+              </Card>
+            )
+          })}
+        </div>
+        :
+        <Flex align="center" gap="middle">
+          <Spin
+            indicator={
+              <LoadingOutlined
+                style={{
+                  fontSize: 48,
+                }}
+                spin
+              />
+            }
+          />
+        </Flex>
+      }
     </MainContainer>
-  )
+  );
 }
--- src/views/Header.js
+++ src/views/Header.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MenuOutlined, UserOutlined } from '@ant-design/icons'
 
 export default function Header() {
+  const [userDropdown, setUserDropdown] = useState(false)
+
   return (
-    <div className='header'>
-      <MenuOutlined className='menu-icon' />
-      <span>SCATTER</span>
-      <UserOutlined className='menu-icon' />
-    </div>
+    <>
+      <div className='header'>
+        <MenuOutlined className='menu-icon' />
+        <span>PAGE NI ISJI</span>
+        <div className='user-btn' onClick={() => setUserDropdown(!userDropdown)}>
+          <UserOutlined className='menu-icon' />
+          {userDropdown &&
+            <div className='user-dropdown'>
+              <div className='dropdown-settings'>My Account</div>
+              <hr />
+              <div className='dropdown-settings'>News Articles</div>
+              <div className='dropdown-settings'>Saved News</div>
+              <div className='dropdown-settings'>Logout</div>
+            </div>
+          }
+        </div>
+      </div>
+    </>
   )
 }
